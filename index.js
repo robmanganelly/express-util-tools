41379d7ad767b9d9d723fcae7c39b29421785b7a
robmanganelly <robmanganelly@gmail.com>
2022-06-13 23:46:40 -0400

    exposing AppError

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {catchAsync, catchWithCustomError} = require('./wrappers/error/error');
 const { envelop }  = require('./wrappers/envelop/envelop');
+const { DefaultAppError } = require('./wrappers/error/defaultAppError');
 
-module.exports = { catchAsync ,catchWithCustomError, envelop };
+module.exports = { catchAsync ,catchWithCustomError, envelop, AppError: DefaultAppError };
